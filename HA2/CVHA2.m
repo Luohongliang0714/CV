4df8413dee5bfbd36926921e676766a6dcf819ee
SimBil91 <Simon.Bilgeri@gmx.de>
2015-05-27 14:05:55 +0200

    HA2

---
+++ HA2/CVHA2.m
@@ -0,0 +1,16 @@
+% Gruppenmitglieder:
+
+%% Bilder laden
+Image1 = imread('szeneL.jpg');
+IGray1 = rgb_to_gray(Image1);
+
+Image2 = imread('szeneR.jpg');
+IGray2 = rgb_to_gray(Image2);
+
+%% Harris-Merkmale berechnen
+Merkmale1 = harris_detektor(IGray1,'k',0.05,'N',100,'min_dist',80,'segment_length',9,'do_plot',false);
+Merkmale2 = harris_detektor(IGray2,'k',0.05,'N',100,'min_dist',80,'segment_length',9,'do_plot',false);
+
+
+%% Korrespondenzschätzung
+% Korrespondenzen = punkt_korrespondenzen(IGray1,IGray2,Merkmale1,Merkmale2);
---
+++ HA2/punkt_korrespondenzen.m
@@ -0,0 +1,5 @@
+function [Korrespondenzen]=punkt_korrespondenzen(I1,I2,Mpt1,Mpt2,varargin)
+% In dieser Funktion sollen Korrespondenzpunktpaare aus den jeweiligen
+% Merkmalspunktlisten berchnet werden
+
+end
